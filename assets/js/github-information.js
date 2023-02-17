a3c788b577ff56e9f058e76573206c7f3f403320
Krnsand <krnsandin@gmail.com>
2023-02-17 09:21:40 +0000

    Add clearfix to css and fetchGitHubInfo to js

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -73,3 +75,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
